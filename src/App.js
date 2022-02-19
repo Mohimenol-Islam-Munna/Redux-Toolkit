b98a35daff162563489377b37509165860f6f1b5
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-02-19 19:36:20 +0600

    redux toolkit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Redux ToolKit Complete Practice</h3>
       </header>
     </div>
   );
