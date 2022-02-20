18903bff47892570628eab8b605676b63ed954a2
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-02-20 10:59:11 +0600

    create store, slice , update state by dispatch action

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Counter from "./Components/Counter";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h3>Redux ToolKit Complete Practice</h3>
       </header>
+
+      <Counter />
     </div>
   );
 }
---
+++ src/Components/Counter.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from "../store/slices/counterSlice";
+
+const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  console.log("counter value", counter);
+
+  return (
+    <div
+      style={{
+        width: "60%",
+        margin: "20px auto",
+        padding: "20px",
+        border: "1px solid salmon",
+        borderRadius: "10px",
+      }}
+    >
+      <h2>Counter {counter.value}</h2>
+      <div>
+        <button
+          style={{
+            padding: "5px",
+            margin: "5px",
+            border: "1px solid cyan",
+            borderRadius: "10px",
+          }}
+          onClick={() => dispatch(increment())}
+        >
+          Increment +
+        </button>
+        <button
+          style={{
+            padding: "5px",
+            margin: "5px",
+            border: "1px solid salmon",
+            borderRadius: "10px",
+          }}
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement -
+        </button>
+
+        <button
+          style={{
+            padding: "5px",
+            margin: "5px",
+            border: "1px solid palegreen",
+            borderRadius: "10px",
+          }}
+          onClick={() => dispatch(incrementByAmount({ value: 5 }))}
+        >
+          Increment By Amount +
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
